acbf54c8fea543ff76e6769cc95e158677177696
Graciano Kevin <93309424+Graciano7@users.noreply.github.com>
2023-02-22 18:52:06 +0700

    Add files via upload

---
+++ 1_D_71210691.py
@@ -0,0 +1,15 @@
+hujan = int(input("Masukkan nilai curah hujan : "))
+try:
+    if hujan >= 100:
+        print("Curah hujan ekstrem")
+    elif hujan >=  51:
+        print("Curah hujan lebat")
+    elif hujan >= 21:
+        print("Curah hujan sedang")
+    elif hujan >= 0.5 :
+        print("Curah hujan ringan")
+    elif hujan >= 0:
+        print("Cuaca terang atau berawan")
+except:
+    print(hujan)
+    
